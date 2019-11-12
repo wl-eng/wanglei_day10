ef67b8febe19cf908e4e9fda05e67e7559c81c18
wl-eng-git <1972932710@qq.com>
2019-11-12 13:16:14 +0800

    123456

---
+++ src/main/java/com/wanglei/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.wanglei.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+	@RequestMapping(value="hello",method=RequestMethod.GET)
+	public String tet(HttpServletRequest request) {
+		request.setAttribute("info","hello");
+		return "user/test";
+	}
+}
